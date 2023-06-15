e4ed56be0d6fece1ead92fe75adfd2e3060c35da
swkim <ksw4997@naver.com>
2023-06-15 14:03:24 +0900

    ADD | nginx 기본환경 세팅

--- docker_test/docker_test/settings.py
+++ docker_test/docker_test/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-o^e$bgz!9hlkh68x$v37-r7dhj8un3d_36y7^64%)n%p9!l_mb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', "website"]
 
 
 # Application definition
@@ -121,3 +122,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
